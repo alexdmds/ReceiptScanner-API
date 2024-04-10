35396b8b7f7834249787994cdb723605b945aa94
alexdmds <38859202+alexdmds@users.noreply.github.com>
2024-04-10 17:02:14 +0200

    API ok sans cv2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from google.cloud import vision
-from PIL import Image
-import io
+from AnalyseSpatiale import get_text_from_image
 
 app = Flask(__name__)
 
@@ -17,20 +16,9 @@ def process_ticket():
     file = request.files['ticket_image']
     content = file.read()
 
-    # Préparez l'image pour Google Cloud Vision
-    image = vision.Image(content=content)
+    texte = get_text_from_image(content)
 
-    # Détectez le texte dans l'image
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-
-    if response.error.message:
-        return jsonify({'error': response.error.message}), 500
-
-    # Extrait le texte de la première annotation (contenu complet)
-    extracted_text = texts[0].description if texts else ''
-
-    return jsonify({'extracted_text': extracted_text})
+    return jsonify({'extracted_text': texte})
 
 if __name__ == '__main__':
     app.run(debug=True)

20b09a7261966fc488751933e9c5ca464ede8c16
alexdmds <38859202+alexdmds@users.noreply.github.com>
2024-10-17 17:52:21 +0200

    last stable version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 from google.cloud import vision
 from AnalyseSpatiale import get_text_from_image
 from AnalyseText import get_structured_json_from_text
@@ -7,18 +8,35 @@ app = Flask(__name__)
 
 @app.route('/api/process-ticket', methods=['POST'])
 def process_ticket():
-    if 'ticket_image' not in request.files:
-        return jsonify({'error': 'No ticket image provided.'}), 400
-    
+    # Vérifier si l'URL de l'image est fournie dans les données JSON de la requête
+    if not request.json or 'image_url' not in request.json:
+        return jsonify({'error': 'No image URL provided.'}), 400
+
+    # Récupérer l'URL de l'image
+    image_url = request.json['image_url']
+
+    # Télécharger l'image à partir de l'URL
+    try:
+        response = requests.get(image_url)
+        # Vérifier si la requête a réussi
+        if response.status_code != 200:
+            return jsonify({'error': 'Failed to download image from URL.'}), 500
+        image_content = response.content
+    except Exception as e:
+        return jsonify({'error': 'Error downloading image from URL: {}'.format(e)}), 500
+
     # Initialisez le client Vision
     client = vision.ImageAnnotatorClient()
 
-    # Lisez l'image depuis le fichier envoyé
-    file = request.files['ticket_image']
-    content = file.read()
+    # Créer un objet Image à partir du contenu de l'image téléchargée
+    image = vision.Image(content=image_content)
 
-    texte = get_text_from_image(content)
+    # Utiliser le client Vision pour détecter du texte dans l'image
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    texte = texts[0].description if texts else ''
 
+    # Convertir le texte en JSON structuré
     response_text = get_structured_json_from_text(texte)
 
     return jsonify(response_text)

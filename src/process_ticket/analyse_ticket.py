0e6ad905e64dc8ef67b93593a2ad07c3da8bc60b
alexdmds <38859202+alexdmds@users.noreply.github.com>
2024-10-31 18:37:00 +0100

    refonte de la structure projet pr test

--- src/process_ticket/analyse_ticket.py
+++ src/process_ticket/analyse_ticket.py
@@ -1,4 +1,4 @@
-from utils.fonction_actuelle import analyse_image
+from process_ticket.fonction_actuelle import analyse_image
 #Ce fichier va contenir la fonction principale appelée dans le main qui prend en entrée une image et qui retourne un json structuré
 #Il appelle d'autres utilitaires pour réaliser les fonctions unitaires
 
--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Ajoute le dossier parent (racine du projet) au chemin Python
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
